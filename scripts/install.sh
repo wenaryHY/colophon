47bad5b5ae6eaa54b4d513c3d8c20008f80dd2da
Wenary <wenary@inkforge.dev>
2026-06-24 04:26:13 +0800

    部署脚本: deploy/colophon.service和install.sh添加WASMTIME_CACHE_DISABLE=1

--- scripts/install.sh
+++ scripts/install.sh
@@ -161,6 +161,7 @@ Restart=always
 RestartSec=5
 
 Environment=RUST_LOG=colophon=info
+Environment=WASMTIME_CACHE_DISABLE=1
 Environment=COLOPHON__RUNTIME__MODE=production
 Environment=COLOPHON__SERVER__HOST=0.0.0.0
 Environment=COLOPHON__SERVER__PORT=${LISTEN_PORT}
